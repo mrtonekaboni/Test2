0026f779faefb8d9ce73e69f12d2085b50ef237f
Mohamadreza Tonekaboni <mr.tonekaboni2001@gmail.com>
2025-05-20 14:11:23 +0330

    added 1

---
+++ test_train.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    try:
+        if isinstance(a, int) and isinstance(b, int):
+            return a + b
+        else:
+            raise ValueError
+    except ValueError as e:
+        print(f"خطا: {e}")
+        return None
+
+
+print(add(5,6))
